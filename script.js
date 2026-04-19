32493e3c141b52bdf1a7554cc3b86201f8745be8
steizy <steizym.fornica2@itjo.edu.ve>
2026-04-19 17:58:44 -0400

    Navbar+footer

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('expanded');
-}
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menuLinks.classList.toggle('active');
+    
+    // Animación simple del icono hamburguesa
+    const bars = document.querySelectorAll('.bar');
+    bars[0].classList.toggle('rotate45');
+    bars[1].classList.toggle('fadeout');
+    bars[2].classList.toggle('rotate-45');
+});
